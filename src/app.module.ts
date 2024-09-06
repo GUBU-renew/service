a4202b6e3dea9da8e6b3bc1c8a70e79e0a41982c
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-09-07 08:26:57 +0900

    [chore/001] 기초파일세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configValidationSchema } from './env-validation.config';
+import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
+import { PlatformModule } from './platform/platform.module';
+import { UserModule } from './user/user.module';
+import { UserSubscriptionModule } from './user-subscription/user-subscription.module';
 
 @Module({
   imports: [
@@ -11,8 +16,14 @@ import { configValidationSchema } from './env-validation.config';
       validationSchema: configValidationSchema,
       envFilePath: '.env',
     }),
+    AuthModule,
+    CategoryModule,
+    PlatformModule,
+    UserModule,
+    UserSubscriptionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+//
